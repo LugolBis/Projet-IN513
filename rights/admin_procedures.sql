9a8720ea0916b8cf3faf585ffca802b13e983952
samarive <ytreza184@gmail.com>
2024-11-21 08:35:00 +0100

    Ajout de la procédure add_user (a tester)

---
+++ rights/admin_procedures.sql
@@ -0,0 +1,8 @@
+
+create or replace procedure add_user(pseudo IN varchar, mail IN varchar, password in VARCHAR) as
+begin
+	insert into Users values (pseudo, null, null, mail, 'unknown', null);
+	execute immediate 'create user ' || pseudo || ' identified by ' || password;
+	execute immediate 'grant Client to ' || pseudo; 
+end;
+/
