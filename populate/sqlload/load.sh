4454eeb3fac41f7e732cab2eb42a0c465d809d69
Sama Rive <ytreza184@gmail.com>
2024-12-10 12:29:34 +0100

    Ajout des scripts de chargement pour la table Post

--- populate/sqlload/load.sh
+++ populate/sqlload/load.sh
@@ -2,4 +2,6 @@
 
 # Peuple la base de donnée à grâce à SQL*Load.
 
-sqlldr userid=admin/banane control=user_control.txt log=user_log.txt bad=user_bad.txt discard=user_disard.txt errors=0 skip=1
+# TODO: Décommenter tout ! (les lignes étaient commentées pour tester)
+# sqlldr userid=admin/banane control=user/control.txt log=user/log.txt bad=user/bad.txt discard=user/disard.txt errors=0 skip=1
+sqlldr userid=admin/banane control=post/control.txt log=post/log.txt bad=post/bad.txt discard=post/disard.txt errors=0 skip=0
