15c6f63020a8c85f870efd5a7c2b662091049616
Sama Rive <ytreza184@gmail.com>
2024-12-13 11:57:40 +0100

    Peuplage table follow

--- populate/sqlload/load.sh
+++ populate/sqlload/load.sh
@@ -12,4 +12,6 @@
 # sqlldr userid=admin/banane control=vote/control.txt log=vote/log.txt bad=vote/bad.txt discard=vote/discard.txt errors=0 skip=1
 # sqlldr userid=admin/banane control=private_message/control.txt log=private_message/log.txt bad=private_message/bad.txt discard=private_message/discard.txt errors=0 skip=1
 # sqlldr userid=admin/banane control=receive/control.txt log=receive/log.txt bad=receive/bad.txt discard=receive/discard.txt errors=0 skip=1
-sqlldr userid=admin/banane control=signal/control.txt log=signal/log.txt bad=signal/bad.txt discard=signal/discard.txt errors=0 skip=1
+# sqlldr userid=admin/banane control=signal/control.txt log=signal/log.txt bad=signal/bad.txt discard=signal/discard.txt errors=0 skip=1
+# sqlldr userid=admin/banane control=draft/control.txt log=draft/log.txt bad=draft/bad.txt discard=draft/discard.txt errors=0 skip=1
+sqlldr userid=admin/banane control=follow/control.txt log=follow/log.txt bad=follow/bad.txt discard=follow/discard.txt errors=0 skip=1
