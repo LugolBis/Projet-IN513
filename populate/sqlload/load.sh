e7ef279ee36186dcc7ef7fafdfd5459e5ba88fe6
Sama Rive <ytreza184@gmail.com>
2024-12-13 10:07:10 +0100

    Ajout des scripts pour peupler la table Options

--- populate/sqlload/load.sh
+++ populate/sqlload/load.sh
@@ -4,5 +4,6 @@
 
 # TODO: Décommenter tout ! (les lignes étaient commentées pour tester)
 # sqlldr userid=admin/banane control=user/control.txt log=user/log.txt bad=user/bad.txt discard=user/disard.txt errors=0 skip=1
-sqlldr userid=admin/banane control=post/control.txt log=post/log.txt bad=post/bad.txt discard=post/disard.txt errors=0 skip=0
-# sqlldr userid=admin/banane control=survey/control.txt log=survey/log.txt bad=survey/bad.txt discard=survey/disard.txt errors=0 skip=1
+# sqlldr userid=admin/banane control=post/control.txt log=post/log.txt bad=post/bad.txt discard=post/disard.txt errors=0 skip=0
+sqlldr userid=admin/banane control=survey/control.txt log=survey/log.txt bad=survey/bad.txt discard=survey/disard.txt errors=0 skip=1
+sqlldr userid=admin/banane control=option/control.txt log=option/log.txt bad=option/bad.txt discard=option/disard.txt errors=0 skip=1
